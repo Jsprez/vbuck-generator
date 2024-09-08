463dcbcbf2101c2e2f5a09a93385b80aadb5a4a4
Jsprez <165509271+Jsprez@users.noreply.github.com>
2024-09-08 19:56:34 +0200

    script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+function startGeneration() {
+    var username = document.getElementById('username').value;
+    if (username) {
+        document.getElementById('generatorSection').style.display = 'block';
+    } else {
+        alert('Veuillez entrer votre pseudo !');
+    }
+}
+
+function generateVbucks() {
+    var amount = prompt('Combien de V-Bucks voulez-vous générer ?');
+    if (amount) {
+        document.getElementById('confirmationMessage').innerText = 
+            'Les ' + amount + ' V-Bucks vont être envoyés à votre compte, en fait non, c\'est juste une blague !';
+        document.getElementById('confirmation').style.display = 'block';
+    }
+}
